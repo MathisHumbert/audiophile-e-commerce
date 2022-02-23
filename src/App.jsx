9437ebdc65956553505c5734f2a50d8a072779b6
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-23 10:18:33 +0100

    react-router-dom set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  Checkout,
+  Earphones,
+  Headphones,
+  Home,
+  Product,
+  Speakers,
+} from './pages';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/headphones' element={<Headphones />} />
+        <Route path='/speakers' element={<Speakers />} />
+        <Route path='/earphones' element={<Earphones />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/product/:productName' element={<Product />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Checkout from './Checkout/Checkout';
+import Earphones from './Earphones/Earphones';
+import Headphones from './Headphones/Headphones';
+import Home from './Home/Home';
+import Product from './Product/Product';
+import Speakers from './Speakers/Speakers';
+
+export { Checkout, Earphones, Headphones, Home, Product, Speakers };
